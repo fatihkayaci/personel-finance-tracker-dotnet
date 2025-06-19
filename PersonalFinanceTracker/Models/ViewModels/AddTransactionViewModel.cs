92c2599bc9289ce9c6dc04de36bd5c344182ded6
Fatih Kayacı <fatihkayaci@yahoo.com>
2025-06-19 15:32:22 +0300

    hem category hemde transaction bilgileri gelsin diye viewmodel oluşturuldu ve Add.cshtml dosyasında gösterildi.

---
+++ PersonalFinanceTracker/Models/ViewModels/AddTransactionViewModel.cs
@@ -0,0 +1,16 @@
+namespace PersonalFinanceTracker.Models.ViewModels
+{
+    public class AddTransactionViewModel
+    {
+         // Transaction bilgileri
+        public decimal Amount { get; set; }
+        public string? Description { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public int TransactionType { get; set; }
+        public int CategoryId { get; set; }
+        
+        // Kategori listeleri
+        public List<CategoryModel> ExpenseCategories { get; set; } = new List<CategoryModel>();
+        public List<CategoryModel> IncomeCategories { get; set; } = new List<CategoryModel>();
+    }
+}
