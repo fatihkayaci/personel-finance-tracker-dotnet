2910cd063883de80a8403c09401f1f3078b79cd1
Fatih Kayacı <fatihkayaci@yahoo.com>
2025-06-19 15:33:03 +0300

    transaction eklenmesi ve viewmodelin gönderilmesi işlemleri yapıldı.

--- PersonalFinanceTracker/Controllers/TransactionController.cs
+++ PersonalFinanceTracker/Controllers/TransactionController.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PersonalFinanceTracker.Interface;
-
+using PersonalFinanceTracker.Models;
+using PersonalFinanceTracker.Models.ViewModels;
+using System.Security.Claims;
 namespace PersonalFinanceTracker.Controllers
 {
+    [Authorize]
     public class TransactionController : Controller
     {
         private readonly ITransactionService _transactionService;
@@ -12,16 +16,58 @@ namespace PersonalFinanceTracker.Controllers
             _transactionService = transactionService;
             _categoryService = categoryService;
         }
+
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var transactions = await _transactionService.GetAllTransactionsAsync();
+            var userId = User.Identity?.Name;
+            if (string.IsNullOrEmpty(userId))
+            {
+                return BadRequest("User not found");
+            }
+            var transactions = await _transactionService.GetTransactionsByUserIdAsync(userId);
             return View(transactions);
         }
-        public IActionResult Add()
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
         {
-            return View();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId)) return BadRequest("user not found!");
+            var viewModel = new AddTransactionViewModel
+            {
+                TransactionDate = DateTime.Now,
+                TransactionType = 0,
+                ExpenseCategories = await _categoryService.GetCategoriesByTypeAsync(0, userId),
+                IncomeCategories = await _categoryService.GetCategoriesByTypeAsync(1, userId),
+            };
+            return View(viewModel);
         }
-        public IActionResult Edit()
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTransactionViewModel model)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId)) return BadRequest("user not found!");
+
+            Console.WriteLine($"Amount: {model.Amount}");
+            Console.WriteLine($"Description: {model.Description}");
+            Console.WriteLine($"TransactionDate: {model.TransactionDate}");
+            Console.WriteLine($"CategoryId: {model.CategoryId}");
+            Console.WriteLine($"transaction type: {model.TransactionType}");
+            var transaction = new TransactionModel
+            {
+                Amount = model.Amount,
+                Description = model.Description,
+                TransactionDate = model.TransactionDate,
+                TransactionType = model.TransactionType,
+                CategoryId = model.CategoryId,
+                UserId = userId,
+                CreatedDate = DateTime.Now
+            };
+            await _transactionService.AddTransactionAsync(transaction);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
         {
             return View();
         }
